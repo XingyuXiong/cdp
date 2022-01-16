ce3c819f27dfe5abb685d787bf4926f2215d8675
nethe-GitHub <78287169+nethe-GitHub@users.noreply.github.com>
2022-01-17 04:39:16 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-(async function init(){
-document.body.innerHTML += '<p id="p_init" class="loadingsign">Initializing</p>\n'
-						+ '<p id="p_webs" class="loadingsign">Connecting</p>\n';
-// initialize data structures
-var PLAYERS = [], PLAYER_TEMPLATE = {};
-var CARDS = [], CARD_TEMPLATE = {};
-var DECKS = [], DECK_TEMPLATE = {};
-var MAXID = 65536;
+var PLAYERS = {}, CARDS = {}, DECKS = {},
+	WEBSOCKET, WEBSOCKET_PATH, USERNAME,
+	MAXZ = 0,
+	REMOVE_FRAME = (() => {});
 
-var USERNAME = String.fromCharCode(Math.random()*26+97,Math.random()*26+97,Math.random()*26+97);
+(async function init(){
+document.getElementById('warning').innerText = "Initializing";
+
 if(document.cookie == ''){
 	USERNAME = prompt("Username:");
+	if(USERNAME == '')
+		USERNAME = String.fromCharCode(Math.random()*26+97,Math.random()*26+97,Math.random()*26+97);
 	document.cookie = USERNAME;
 } else {
 	USERNAME = document.cookie;
 }
 
-document.getElementById("p_init").remove();
-
-// initialize websocket connection
 async function retry(func, errfunc, time){
 	while(true){
 		try{
@@ -29,69 +26,33 @@ async function retry(func, errfunc, time){
 		}
 	}
 }
-function onreceive(ev){
-	let m = JSON.parse(ev.data.toString());
-	if(m['type'] == 'script'){
-		//console.log(m['content']);
-		(new Function(m['content']))(); //using "eval(m['data']);" is too slow
-	} else if(m['type'] == 'data'){
-		
-		// to be continued...
-		
-	}
-}
 
-var WEBSOCKET, WEBSOCKET_PATH = "ws" + `${(location.protocol === 'https:') ? 's' : ''}` + "://" + `${document.location.hostname}` + ":" + `${document.location.port}`;
-document.getElementById("p_webs").innerText = "Connecting to " + WEBSOCKET_PATH;
+WEBSOCKET_PATH = "ws" + `${(location.protocol === 'https:') ? 's' : ''}` + "://" + `${document.location.hostname}` + ":" + `${document.location.port}`;
 
+document.getElementById('warning').innerText = "Connecting to " + WEBSOCKET_PATH;
 await retry( () => {
 	WEBSOCKET = new WebSocket(WEBSOCKET_PATH);
-	WEBSOCKET.onmessage = onreceive;
 }, (ex) => {
 	console.log(ex);
 	//alert("Connection failed!\n");
-	document.getElementById("p_webs").innerText = "Unable to connect, retrying...";
+	document.getElementById('warning').innerText = "Unable to connect " + WEBSOCKET_PATH + ", retrying...";
 }, 3000);
 
+WEBSOCKET.onmessage = ((ev) => {
+	var m = JSON.parse(ev.data.toString());
+	var ce = new CustomEvent('_' + m['type'], {"detail":m['content']});
+	//console.log(ce);
+	document.dispatchEvent(ce);
+});
+// custom events name begin with '_'
+document.addEventListener('_script', (e) => { new Function(e.detail)(); });
+
 await retry( () => {
+	document.getElementById('warning').innerText = "";
 	WEBSOCKET.send(`{"type":"login","content":"${USERNAME}"}`);
-}, (ex) => {}, 200);
-
-document.getElementById("p_webs").remove();
-
-//test 
-
-document.body.children[0].style.zIndex = 0; // maxz
-function gendeck(){
-	var WINDOW_WIDTH = document.documentElement.clientWidth;
-	var WINDOW_HEIGHT = document.documentElement.clientHeight;
-	var CARD_WIDTH = Math.round(Math.min(WINDOW_WIDTH, WINDOW_HEIGHT) / 7);
-	var CARD_HEIGHT = Math.round(CARD_WIDTH * 1.4992);
-	
-	let card_list = ['JB', 'JR'];
-	['S','H','C','D'].forEach( (s, i) => { 
-		['A','2','3','4','5','6','7','8','9','T','J','Q','K'].forEach( (r, j) => {
-			card_list.push(s + r); }); });
-	card_list.forEach( (c, k) => {
-		let card = document.createElement("span");
-		card.id = ++MAXID;
-		card.classList.add("Card");
-		card.classList.add(c);
-		card.style.left = Math.random() * (WINDOW_WIDTH - CARD_WIDTH) + "px";
-		card.style.top = Math.random() * (WINDOW_HEIGHT - CARD_HEIGHT) + "px";
-		card.style.backgroundColor = "hsla(0,0%,90%,0.96)";
-		card.style.zIndex = ++document.body.children[0].style.zIndex;
-		document.body.appendChild(card);
-	});
-	return true;
-}
-retry( () => {
-	if(document.getElementById("css_frame") === null
-	|| document.getElementById("css_cards") === null)
-		throw "not ready";
-	gendeck()
 }, (ex) => {
-}, 100);
+	document.getElementById('warning').innerText = "Logging in...";
+}, 200);
 
 
 })();
--- server.js
+++ server.js
@@ -1,55 +1,92 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
-const WebSocket = require('ws');
+const EVENTS = require('events');
+const HTTP = require("http");
+const URL = require("url");
+const FS = require("fs");
+const WS = require('ws');
 
-record = "";
+////////////////////////////// test /////////////////////////////////////
+//                                                                     //
+var card_type = ['JB', 'JR'];
+['S','H','C','D'].forEach( (s, i) => { 
+	['A','2','3','4','5','6','7','8','9','T','J','Q','K'].forEach( (r, j) => {
+		card_type.push(s + r); }); });
+var card_list = {};
+card_type.forEach( (t, i) => {
+	card_list['c'+i] = { type:t,
+						 loc:{ pile:'desk',
+							   pos:{ x:100*Math.random()-10,
+									 y:100*Math.random()-10
+} } } });
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
 
-function getHTML(res){
-    fs.readFile('script.js', (err, jsdata) => {
-        if(err) return console.log(err);
-        fs.readFile('cdp.html', (err, htmldata) => {
-			if(err) return console.log(err);
-			let script = "<script>\n" + jsdata.toString() + "\n</script>";
-			let html = htmldata.toString().replace("<script src=\"script.js\"></script>", script);
-			res.end(html);
-		});
-    });
-}
-
-function sendScript(conn, path){
-    fs.readFile(path, (err, text) => {
-        if(err) return console.log(err);
-		let t = { type:'script', content:text.toString()};
-        conn.send(JSON.stringify(t));
-    });
-}
-
-const hserver = http.createServer( (request, response) => {
+const hserver = HTTP.createServer( (request, response) => {
 	response.writeHead(200, {"Content-Type": "text/html"});
     getHTML(response);
 }).listen(8080);
-const wsserver = new WebSocket.Server({ server: hserver });
+const wsserver = new WS.Server({ server: hserver });
 
+
+const eventEmitter = new EVENTS.EventEmitter();
 wsserver.on('connection', (conn) => {
 	console.log("new connection");
 	conn.on('message', ((theconn) => {
-		// use closure to reply more easily
-		return (msg) => { onreceive(theconn, msg); }; 
+		return ((msg) => {
+			var m = JSON.parse(msg.toString());
+			eventEmitter.emit(m['type'], theconn, m['content']);
+		});
 	})(conn));
 	conn.on('close', () => { console.log("connection lost"); });
 	conn.on('error', (ev) => { console.log('error' + ev); });
 });
 
-function onreceive(conn, msg){
-	let m = JSON.parse(msg.toString());
-	if(m['type'] == 'login'){
-		console.log(m['content'] + " has just logged in!");
-		sendScript(conn, "frame\\default.js");
-		sendScript(conn, "cards\\default.js");
-	} else if(m['type'] == 'data'){
-		
-		// to be continued...
-		
-	}
+
+eventEmitter.on('login', (conn, username) => {
+	console.log(username + " has just logged in!");
+	sendScript(conn, "frame\\default.js");
+	sendScript(conn, "cards\\default.js");
+});
+function getHTML(res){
+    FS.readFile('script.js', (err, jsdata) => {
+        if(err) return console.log(err);
+        FS.readFile('cdp.html', (err, htmldata) => {
+			if(err) return console.log(err);
+			var script = ">\n" + jsdata.toString() + "\n";
+			var html = htmldata.toString().replace('src="script.js">', script);
+			res.end(html);
+		});
+    });
 }
+function sendScript(conn, path){
+    FS.readFile(path, (err, text) => {
+        if(err) return console.log(err);
+		var t = { type:'script', content:text.toString() };
+        conn.send(JSON.stringify(t));
+    });
+}
+////////////////////////////// test /////////////////////////////////////
+//                                                                     //
+eventEmitter.on('ready', (conn, _) => {
+	var t = { type:'sync', content:card_list };
+	conn.send(JSON.stringify(t));
+});
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
+
+
+eventEmitter.on('chat', (conn, text) => {
+	var username = "xjp";
+	console.log(username + " says: " + text);
+	var t = { type:'chat', content:{ username:username, text:text } };
+	wsserver.clients.forEach( (cli) => { cli.send(JSON.stringify(t)); });
+});
+
+////////////////////////////// test /////////////////////////////////////
+//                                                                     //
+eventEmitter.on('move', (conn, data) => {
+	data.cards.forEach( (id, _) => { card_list[id].loc = data.toloc; });
+	var t = { type:'move', content:data };
+	wsserver.clients.forEach( (cli) => { cli.send(JSON.stringify(t)); });
+});
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
